85f930f1f8342832dbecd59265285348156c6c61
Igor Pashev <pashev.igor@gmail.com>
2014-09-01 02:40:22 +0400

    Added missed include bits/libc-lock.h
    For __libc_lock_* macros

--- sysdeps/unix/sysv/solaris2/kopensolaris-gnu/priv.c
+++ sysdeps/unix/sysv/solaris2/kopensolaris-gnu/priv.c
@@ -17,6 +17,7 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
+#include <bits/libc-lock.h>
 #include <inline-syscall.h>
 #include <privP.h>
 #include <sys/priocntl.h>
